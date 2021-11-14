ba7ab46989e7b928acdf6963b349d2d3ff5a8ca1
PZVBCB@std.uni-eszterhazy.hu <PZVBCB@std.uni-eszterhazy.hu>
2021-11-14 21:02:36 +0100

    123

--- házifeladat/Form1.cs
+++ házifeladat/Form1.cs
@@ -19,34 +19,47 @@ namespace házifeladat
             InitializeComponent();
         }
 
+        public double ToDeg(double x)
+        {
+            return x;
+        }
         private void Form1_Paint(object sender, PaintEventArgs e)
         {
         }
-        public void rekFa(float n, int x, int y, float szog, int len, int branchNum )
+        private void pictureBox1_Paint(object sender, PaintEventArgs e)
         {
-            if(n < 0)
+            int x = pictureBox1.Width;
+            int y = pictureBox1.Height;
+            g = e.Graphics;
+            rekFa(5,
+                x / 2,
+                y / 2,
+                90,
+                40,
+                3);
+        }
+        public void rekFa(float n, float x, float y, double szog, int len, int branchNum)
+        {
+            if (n < 0)
             {
                 return;
             }
             var rnd = new Random();
-            int C = rnd.Next(-45, 45);
 
-            
+            double ph1 = Math.Round(Math.Cos(szog * Math.PI / 180), 6);
+            double ph2 = Math.Round(Math.Sin(szog * Math.PI / 180), 6);
+
             PointF p1 = new PointF(x, y);
-            PointF p2 = new PointF((int)(x + (len * Math.Cos(szog))), (int)(y + (len * Math.Sin(szog))));
+            PointF p2 = new PointF((float)(x + len * ph1), (float)(y - len * ph2));
 
             g.DrawLine(Pens.Red, p1, p2);
 
-                for (int i = 0; i <= branchNum; i++)
-                {
-                    rekFa(n - 1, (int)p2.X, (int)p2.Y, szog + C, len - 5, branchNum);
-                }
-        }
+            for (int i = 0; i < branchNum; i++)
+            {
+                int C = rnd.Next(-45, 45);
 
-        private void pictureBox1_Paint(object sender, PaintEventArgs e)
-        {
-            g = e.Graphics;
-            rekFa(5, pictureBox1.Width / 2, pictureBox1.Height / 2, 60, 50, 3);
+                rekFa(n - 1, (int)p2.X, (int)p2.Y, szog + C, len - 5, branchNum);
+            }
         }
     }
 }
